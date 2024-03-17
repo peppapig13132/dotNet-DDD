1f9f196724cf561331b9fb176a3f1916032a32bf
peppapig13132 <peppapig13132@gmail.com>
2024-03-17 13:29:33 +0100

    Upload database file: Users table

---
+++ dbo.Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]     INT      IDENTITY(1,1)    NOT NULL,
+    [name]   VARCHAR (50) NULL,
+    [email]  VARCHAR (50) NULL,
+    [gender] VARCHAR (50) NULL,
+    [age]    INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
